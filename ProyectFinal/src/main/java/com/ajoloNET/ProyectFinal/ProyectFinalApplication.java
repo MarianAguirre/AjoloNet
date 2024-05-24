01838a33a323cfcc711a6fccc859dd3b382d6b25
Cristo <cermija@gmail.com>
2024-05-24 13:17:46 -0600

    Primer commit
    Inicio del proyecto de estadías

---
+++ ProyectFinal/src/main/java/com/ajoloNET/ProyectFinal/ProyectFinalApplication.java
@@ -0,0 +1,13 @@
+package com.ajoloNET.ProyectFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectFinalApplication.class, args);
+	}
+
+}
---
+++ ProyectFinal/src/test/java/com/ajoloNET/ProyectFinal/ProyectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajoloNET.ProyectFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
