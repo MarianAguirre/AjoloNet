7f0cb237762e4e5d988f337b8c4e60916b7a479f
MarianAguirre <Marian.Aguirre.Carrill@gmail.com>
2024-07-02 12:21:56 -0600

    Todo igual excepto lo de los pdfs
    Se rescato un commit donde no da el error del duplicado pero se elimino lo agregado con los pdfs

--- Fronted/src/app/shared/pages/profile-page/profile-page.component.ts
+++ Fronted/src/app/shared/pages/profile-page/profile-page.component.ts
@@ -1,71 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DatosUser } from '../../../interfaces/user.interfaces';
+import { EquiposServices } from '../../services/equipos.service';
+import { HttpClient } from '@angular/common/http';
+import { enavironments } from '../../../../environments/envarionments';
 
 @Component({
   selector: 'profile-page',
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
-export class ProfilePageComponent{
+export class ProfilePageComponent implements OnInit {
+  errorMessage: string = "";
+  user!: DatosUser;
+  usuario!:DatosUser
+  baseUrl  = enavironments.baseUrl
 
-  // errorMessage:String="";
-  // user?:User;
-  // userLoginOn:boolean=false;
-  // editMode:boolean=false;
+  constructor(private equipoService: EquiposServices, private http: HttpClient) {}
 
-  // registerForm=this.formBuilder.group({
-  //   id:[''],
-  //   lastname:['',Validators.required],
-  //   firstname:['', Validators.required],
-  // })
-
-  // constructor(private userService:UserService, private formBuilder:FormBuilder, private loginService:LoginService  ){
-  //   this.userService.getUser(this.user?.id!).subscribe({
-  //     next: (userData) => {
-  //       this.user=userData;
-  //       this.registerForm.controls.id.setValue(userData.id!.toString());
-  //       this.registerForm.controls.firstname.setValue( userData.firstname);
-  //       this.registerForm.controls.lastname.setValue( userData.lastname);
-  //     },
-  //     error: (errorData) => {
-  //       this.errorMessage=errorData
-  //     },
-  //     complete: () => {
-  //       console.info("User Data ok");
-  //     }
-  //   })
-
-  //   this.loginService.userLoginOn.subscribe({
-  //     next:(userLoginOn) => {
-  //       this.userLoginOn=userLoginOn;
-  //     }
-  //   })
-
-  // }
-
-  // get firstname()
-  // {
-  //   return this.registerForm.controls.firstname;
-  // }
-
-  // get lastname()
-  // {
-  //   return this.registerForm.controls.lastname;
-  // }
+  ngOnInit(): void {
+    this.equipoService.getUserDatos().subscribe(
+      (response: DatosUser) => {
+        console.log('Datos recibidos:', response); // Verifica la estructura de los datos
+        this.user = response; // Asignar directamente a user
+      },
+    );
+  }
 
 
-  // savePersonalDetailsData()
-  // {
-  //   if (this.registerForm.valid)
-  //   {
-  //     this.userService.updateUser(this.registerForm.value as unknown as User).subscribe({
-  //       next:() => {
-  //         this.editMode=false;
-  //         this.user=this.registerForm.value as unknown as User;
-  //       },
-  //       error:(errorData)=> console.error(errorData)
-  //     })
-  //   }
-  // }
-
+  save(): void {
+    // if (!this.user.id) {
+    //   Swal.fire(
+    //     'Error',
+    //     'El ID del usuario es indefinido.',
+    //     'error'
+    //   );
+    //   return;
+    // }
+    // Swal.fire({
+    //   title: '¿Haz rellenado todos los campos?',
+    //   text: `Al actualizar tu usuario debes asegurarte de rellenar todos los campos`,
+    //   icon: 'warning',
+    //   showCancelButton:true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Sí, actualiza'
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    //     this.equipoService.updateUserDatos(this.user.id!, this.user).subscribe(
+    //       () => {
+    //         this.user = this.user.map(d => d.id === this.user.id ? this.user : d);
+    //         Swal.fire(
+    //           'Actualizado!',
+    //           'El usuario ha sido actualizado.',
+    //           'success'
+    //         );
+    //       },
+    //       (error: any) => {
+    //         Swal.fire(
+    //           'Error',
+    //           'Hubo un problema al actualizar el usuario.',
+    //           'error'
+    //         );
+    //       }
+    //     );
+    //   }
+    // });
+  }
 }
--- Fronted/src/app/shared/services/equipos.service.ts
+++ Fronted/src/app/shared/services/equipos.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { enavironments } from '../../../environments/envarionments';
+import { DatosUser, User } from '../../interfaces/user.interfaces';
 
 
 
@@ -142,5 +143,23 @@ export class EquiposServices {
     return this.http.get<Device[]>(`${this.baseUrl}/${deviceType}/id/${id}`);
   }
 
+
+
+  getUser(){
+    // /api/admin/users
+    // /api/admin/update/{id}
+    // api/user/update/{id}
+    // api/users/profile
+    // api/user/me
+      }
+
+      getUsers():Observable<User>{
+        return this.http.get<User>(`${this.baseUrl}/admin/users`)
+      }
+
+
+  getUserDatos():Observable<DatosUser>{
+    return this.http.get<DatosUser>(`${this.baseUrl}/user/me`)
+  }
 }
 
